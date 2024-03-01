b99fc6d098458a0aa507b0977a8d529b289d3a67
agcarvalho.eng <agcarvalho.eng@gmail.com>
2024-02-29 21:44:08 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifto/aula02/Aula1JPAApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.aula02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula1JPAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula1JPAApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+insert into produto (descricao, valor) values ('Água sem gás', 5.00);
+insert into produto (descricao, valor) values ('Guaraná Kuat - 2L', 8.50);
+insert into produto (descricao, valor) values ('Fanta', 7.00);
+insert into produto (descricao, valor) values ('Suco uva', 6.50);
+insert into produto (descricao, valor) values ('Vodka Smirnoff', 9.00);
+insert into produto (descricao, valor) values ('Vodka Orloff', 8.50);
+insert into produto (descricao, valor) values ('Coca Cola - 2L', 10.50);
+insert into produto (descricao, valor) values ('Guaraná Antártica - 2L', 9.50);
+insert into produto (descricao, valor) values ('Água com gás', 6.00);
+insert into produto (descricao, valor) values ('Suco maracujá', 6.80);
